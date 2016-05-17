a9e743ed8b767ba8d2580416fd211cd17064d05f
monster-hui <373000945@qq.com>
2016-05-17 21:12:54 +0800

    add  function
    补充函数

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,12 @@
 function calculate_median(arr) {
   //请勿改动函数名
+   var evenArray=[];
+  for(var i=1,j=0;i<arr.length;i=i+2,j++){
+	  evenArray[j]=arr[i];	     
+	  }
+  evenArray.sort(); 
+  var half=(evenArray.length)/2;
+  return (evenArray.length)%2==0? (evenArray[half-1]+evenArray[half])/2: evenArray[half];   
 }
 
 module.exports = calculate_median;
